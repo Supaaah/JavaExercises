1b2a6bb49ba81677e5c2ec2b442b3243a70a86ad
Supaaah <102640212+Supaaah@users.noreply.github.com>
2024-10-29 13:33:13 +0800

    Create PrintSpecialCharacter.java

---
+++ src/JavaPrograms/Looping/PrintSpecialCharacter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintSpecialCharacter{
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Enter a sentence with special character: ");
+        String sentence = scanner.nextLine();
+        for(int i = 0; i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            if(!((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) && !(ch >= '0' && ch <= '9')){
+                System.out.println(ch);
+            }
+        }
+    }
+    
+}
