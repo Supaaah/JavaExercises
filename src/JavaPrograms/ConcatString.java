dcd1bcc8b808eb8cd6f8c15d02410c12e2bb69f0
Supaaah <102640212+Supaaah@users.noreply.github.com>
2024-10-30 11:35:16 +0800

    Update ConcatString.java

--- src/JavaPrograms/ConcatString.java
+++ src/JavaPrograms/ConcatString.java
@@ -1,10 +1,8 @@
 public class ConcatTwoString{
     
     public static void main(String [] args){
-        
         String string1 = "Hello";
         String string2 = "World!";
-        
         System.out.print(string1+" "+string2);
     }
 }
