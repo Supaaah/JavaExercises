c5a64c656a7b517ecb5b66ef0d77c117de31a4d4
Supaaah <102640212+Supaaah@users.noreply.github.com>
2024-10-30 11:22:07 +0800

    Create ConcatString.java

---
+++ src/JavaPrograms/ConcatString.java
@@ -0,0 +1,10 @@
+public class ConcatString{
+    
+    public static void main(String [] args){
+        
+        String string1 = "Hello";
+        String string2 = "World!";
+        
+        System.out.print(string1.concat(string2));
+    }
+}
